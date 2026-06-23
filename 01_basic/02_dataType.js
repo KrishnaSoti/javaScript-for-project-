dde6e05236bafb3b6b1e84bfb3abf9b50be6ee5e
KrishnaSoti <krishna.1.soti@gmail.com>
2026-06-23 17:34:44 +0000

    second_day

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,13 @@
+let name="Krishna"
+let age=19
+let logged=true
+let check=null  //null is a stand alone value---> representation of empty value
+//undefined--> only created a variable didnt give any value;
+//symbol-->uniqueness ke liye mainly used in react;
+//typeof tell you the type of variable you used;
+let str
+
+console.log(typeof check);//object
+console.log(typeof age)
+console.log(typeof undefined)
+
